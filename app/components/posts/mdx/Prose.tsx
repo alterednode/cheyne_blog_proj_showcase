7ee92185dfff910f11b1688ae21b9ee2b65b0ad3
alterednode <75056546+alterednode@users.noreply.github.com>
2026-02-28 13:06:43 -0800

    add some gfm styling back

--- app/components/posts/mdx/Prose.tsx
+++ app/components/posts/mdx/Prose.tsx
@@ -6,7 +6,8 @@ interface ProseProps {
 
 export function Prose({ children }: ProseProps) {
   return (
-    <div className="max-w-[72ch] mx-auto space-y-6 leading-relaxed text-foreground">
+    //TODO: find a better way to handle tailwind stripping the remark-gfm styling
+    <div className="max-w-[72ch] mx-auto space-y-6 leading-relaxed text-foreground [&_ul]:my-4 [&_ul]:list-disc [&_ul]:pl-6 [&_ol]:my-4 [&_ol]:list-decimal [&_ol]:pl-6 [&_li]:my-1 [&_ul.contains-task-list]:list-none [&_ul.contains-task-list]:pl-0 [&_li.task-list-item]:list-none [&_li.task-list-item]:pl-0 [&_li.task-list-item>input]:mr-2">
       {/* Anchor for TOC introduction link
       Add here to have a scroll target for the introduction link in the TOC
       */}
