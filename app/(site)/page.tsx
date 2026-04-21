32e31aef2f3c074d7d16aa071425534b8f73a9de
alterednode <75056546+alterednode@users.noreply.github.com>
2026-04-21 11:31:10 -0700

    got an internsip (!!! Yippee)

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -105,9 +105,9 @@ export default function Home() {
               {/* <p>
                 Feel free to contact me if you&apos;d like to connect!
               </p> */}
-              <p className="font-semibold">
+              {/* <p className="font-semibold">
                 I&apos;m currently looking for co-op or internship opportunities for Summer 2026, so if you have or know of any, please reach out!
-              </p>
+              </p> */}
             </div>
 
             {/* Accent callout */}
